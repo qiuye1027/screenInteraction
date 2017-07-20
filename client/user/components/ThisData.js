73306480fe41050819bda11627350c1dcdf4d94a
supermars01 <kings_821113@163.com>
2017-07-20 17:41:40 +0800

    user

--- client/user/components/ThisData.js
+++ client/user/components/ThisData.js
@@ -1,47 +1,169 @@
 import React, {Component} from 'react' 
+import styles from '../sass/ThisData'
 class ThisData extends Component {
     constructor() {
         super()
         this.state = {
-          thisData:{}
+          thisData:{},
+          editArr:{
+            id:'用户ID',
+            username:'姓名',
+            remark:'备注',
+            password:'密码设置',
+            create_time:'有效期',
+            effective_time:'有效时间（天）',
+            ExpTime:'有效时间',
+            power:'权限设置',
+            tlmg:'终端管理'
+          }
         }
     }
+    handleChange(event) {
+      let _self = this;
+      let name = event.target.name;
+      let data = this.state.thisData;
+      data[name] = event.target.value;
+      this.setState({thisData: data});
+    }
+
     componentDidMount() {
     }
 
     componentWillReceiveProps(nextProps){
-      let _self = this;
-      // console.log(nextProps)
-      this.setState({thisData : nextProps.data});
+      this.setState({thisData:nextProps.data});
     }
 
+    shouldcomponentupdate(){
+      return true;
+    }
 
     render() {
       let _self = this;
-      // console.log(_self.state.thisData);
-      
+      let formDom1 = [<div className="field">
+                <label>用户ID</label>
+                <input type="text" name="id" disabled placeholder="请输入用户ID" />
+              </div>,
+              <div className="field">
+                <label>用户名</label>
+                <div className="ui icon input">
+                  <input type="text" name="username" disabled className={styles.input} placeholder="请输入姓名" />
+                  <i className="user icon"></i>
+                </div>
+              </div>,
+              <div className="field">
+                <label>密码设置</label>
+                <div className="ui icon input">
+                  <input type="password" name="password" disabled className={styles.input} placeholder="请输入密码" />
+                  <i className="lock icon"></i>
+                </div>
+              </div>,
+              <div className="field">
+                <label>备注</label>
+                <input type="text" name="remark" disabled placeholder="请输入备注" />
+              </div>,
+              <div className="two fields">
+                <div className="field">
+                  <label>有效期</label>
+                  <input type="text" name="create_time" disabled placeholder="有效期" />
+                </div>
+                <div className="field">
+                  <label>有效时间（天）</label>
+                  <input type="text" name="effective_time" disabled placeholder="有效时间（天）" />
+                </div>
+              </div>,
+              <div className="field">
+                <label>权限设置</label>
+                <input type="text" name="power" disabled placeholder="权限设置" />
+              </div>,
+              <div className="field">
+                <label>终端管理</label>
+                <input type="text" name="tlmg" disabled placeholder="终端管理" />
+              </div>];
+      let formDom = [];
+      for(var o in this.state.thisData){
+        if(o == 'id'){
+          formDom.push(
+            <div className="field">
+                  <label>{_self.state.editArr[o]}</label>
+                  <input type="text" name={o} disabled value={_self.state.thisData[o]} placeholder={o} onChange={this.handleChange.bind(this)} />
+            </div>
+          )
+        }else if(o == 'username'){
+          formDom.push(
+            <div className="field">
+              <label>{_self.state.editArr[o]}</label>
+              <div className="ui icon input">
+                <input type="text" name={o} className={styles.input} value={_self.state.thisData[o]} placeholder={o} onChange={this.handleChange.bind(this)} />
+                <i className="user icon"></i>
+              </div>
+            </div>
+          )
+        }else if(o == 'password'){
+          formDom.push(  
+            <div className="field">
+              <label>{_self.state.editArr[o]}</label>
+              <div className="ui icon input">
+                <input type="password" name={o} className={styles.input} value={_self.state.thisData[o]} placeholder={o} onChange={this.handleChange.bind(this)} />
+                <i className="lock icon"></i>
+              </div>
+            </div>
+          )
+        }else{
+          formDom.push(
+            <div className="field">
+                  <label>{_self.state.editArr[o]}</label>
+                  <input type="text" name={o} value={_self.state.thisData[o]} placeholder={o} onChange={this.handleChange.bind(this)} />
+            </div>
+          )  
+        }
+      }
       return (
              <form className="ui form">
-              <div className="field">
-                <label>kid</label>
-                <input type="text" name="kid" value={_self.state.thisData.kid} placeholder="kid" />
-              </div>
-              <div className="field">
-                <label>kname</label>
-                <input type="text" name="kname" value={_self.state.thisData.kname} placeholder="kname" />
-              </div>
-              <div className="field">
-                <label>hasChild</label>
-                <input type="text" name="hasChild" value={_self.state.thisData.hasChild} placeholder="hasChild" />
-              </div>
-              <div className="field">
-                <label>pid</label>
-                <input type="text" name="pid" value={_self.state.thisData.pid} placeholder="pid" />
-              </div>
+              {formDom}
               <button className="ui button" type="button">确定修改</button>
-            </form>
+             </form>
       )
     }
 }
 
 export default ThisData
+              // <div className="field">
+              //   <label>{_self.state.editArr.id}</label>
+              //   <input type="text" name="id" value={_self.state.thisData.id} disabled placeholder="id" onChange={this.handleChange.bind(this)} />
+              // </div>
+              // <div className="field">
+              //   <label>{_self.state.editArr.username}</label>
+              //   <div className="ui icon input">
+              //     <input type="text" name="username" className={styles.input} value={_self.state.thisData.username} placeholder="username" onChange={this.handleChange.bind(this)} />
+              //     <i className="user icon"></i>
+              //   </div>
+              // </div>
+              // <div className="field">
+              //   <label>{_self.state.editArr.password}</label>
+              //   <div className="ui icon input">
+              //     <input type="password" name="password" className={styles.input} value={_self.state.thisData.password} placeholder="password" onChange={this.handleChange.bind(this)} />
+              //     <i className="lock icon"></i>
+              //   </div>
+              // </div>
+              // <div className="field">
+              //   <label>{_self.state.editArr.remark}</label>
+              //   <input type="text" name="remark" value={_self.state.thisData.remark} placeholder="remark" onChange={this.handleChange.bind(this)} />
+              // </div>
+              // <div className="two fields">
+              //   <div className="field">
+              //     <label>{_self.state.editArr.create_time}</label>
+              //     <input type="text" name="create_time" value={_self.state.thisData.create_time} placeholder="create_time" onChange={this.handleChange.bind(this)} />
+              //   </div>
+              //   <div className="field">
+              //     <label>{_self.state.editArr.effective_time}</label>
+              //     <input type="text" name="effective_time" value={_self.state.thisData.effective_time} placeholder="effective_time" onChange={this.handleChange.bind(this)} />
+              //   </div>
+              // </div>
+              // <div className="field">
+              //   <label>{_self.state.editArr.power}</label>
+              //   <input type="text" name="power" value={_self.state.thisData.power} placeholder="power" onChange={this.handleChange.bind(this)} />
+              // </div>,
+              // <div className="field">
+              //   <label>{_self.state.editArr.tlmg}</label>
+              //   <input type="text" name="tlmg" value={_self.state.thisData.tlmg} placeholder="tlmg" onChange={this.handleChange.bind(this)} />
+              // </div>
--- client/user/components/kpiTree.js
+++ client/user/components/kpiTree.js
@@ -10,102 +10,80 @@ export default class KpiTree extends Component{
         super(props);  
         this.state={  
                 data:[  
-                  {"kid":"1","kname":"综合指标","hasChild":"1","pid":"-1"},  
+                  {
+                    "kid":"1",
+                    "kname":"综合指标",
+                    "hasChild":"1",
+                    "pid":"-1",
+                    "id":'1',
+                    "username":'张三1',
+                    "password":'123456',
+                    "remark":'李四死敌1',
+                    "create_time":"2017-01-01",
+                    "effective_time":1,
+                    "power":'权限设置1',
+                    "tlmg":'终端管理1'
+                  },  
                   {"kid":"2","kname":"分析类指标","hasChild":"1","pid":"-1"},  
                   {"kid":"3","kname":"组合指标","hasChild":"1","pid":"-1"},  
                   {"kid":"4","kname":"移动业务计费收入","hasChild":"1","pid":"-1"},  
                   {"kid":"5","kname":"2G业务","hasChild":"1","pid":"-1"},  
                 ],
                 data1:[  
-                  {"kid":"01","kname":"综合指标1","hasChild":"1","pid":"-1"},  
+                  {
+                    "kid":"01",
+                    "kname":"综合指标",
+                    "hasChild":"1",
+                    "pid":"-1",
+                    "id":'01',
+                    "username":'张三01',
+                    "password":'123456',
+                    "remark":'李四死敌01',
+                    "create_time":"2017-01-011",
+                    "effective_time":11,
+                    "power":'权限设置01',
+                    "tlmg":'终端管理01'
+                  },  
                   {"kid":"02","kname":"分析类指标2","hasChild":"1","pid":"-1"},  
                   {"kid":"03","kname":"组合指标3","hasChild":"1","pid":"-1"},  
                   {"kid":"04","kname":"移动业务计费收入4","hasChild":"1","pid":"-1"},  
                   {"kid":"05","kname":"2G业务5","hasChild":"1","pid":"-1"}  
                 ],
                 data01:[  
-                  {"kid":"011","kname":"综合指标01","hasChild":"1","pid":"01"},  
-                  {"kid":"012","kname":"分析类指标02","hasChild":"1","pid":"01"},  
-                  {"kid":"013","kname":"组合指标03","hasChild":"1","pid":"01"},  
-                  {"kid":"014","kname":"移动业务计费收入04","hasChild":"1","pid":"01"},  
-                  {"kid":"015","kname":"2G业务05","hasChild":"1","pid":"01"}  
+                  {
+                    "kid":"001",
+                    "kname":"综合指标",
+                    "hasChild":"1",
+                    "pid":"-1",
+                    "id":'011',
+                    "username":'张三011',
+                    "password":'123456',
+                    "remark":'李四死敌011',
+                    "create_time":"2017-01-0111",
+                    "effective_time":111,
+                    "power":'权限设置011',
+                    "tlmg":'终端管理011'
+                  },  
+                  {"kid":"002","kname":"分析类指标02","hasChild":"1","pid":"01"},  
+                  {"kid":"003","kname":"组合指标03","hasChild":"1","pid":"01"},  
+                  {"kid":"004","kname":"移动业务计费收入04","hasChild":"1","pid":"01"},  
+                  {"kid":"005","kname":"2G业务05","hasChild":"1","pid":"01"}  
                 ],
-                data011:[
-                  {"kid":"0111","kname":"综合指标000","hasChild":"0","pid":"010"},  
-                  {"kid":"0112","kname":"分析类指标000","hasChild":"0","pid":"010"},  
-                  {"kid":"0113","kname":"组合指标000","hasChild":"0","pid":"010"},  
-                  {"kid":"0114","kname":"移动业务计费收入000","hasChild":"0","pid":"010"},  
-                  {"kid":"0115","kname":"2G业务000","hasChild":"0","pid":"010"}  
+                data001:[
+                  {"kid":"0001","kname":"综合指标000","hasChild":"0","pid":"010"},  
+                  {"kid":"0002","kname":"分析类指标000","hasChild":"0","pid":"010"},  
+                  {"kid":"0003","kname":"组合指标000","hasChild":"0","pid":"010"},  
+                  {"kid":"0004","kname":"移动业务计费收入000","hasChild":"0","pid":"010"},  
+                  {"kid":"0005","kname":"2G业务000","hasChild":"0","pid":"010"}  
                 ],
                 thisData:{},
         }  
-        this._handleSelect=this._handleSelect.bind(this);  
-        this._handleSearch=this._handleSearch.bind(this);  
-        this._handleReturn=this._handleReturn.bind(this);  
-    }  
-    _handleSearch=()=>{   
-        var _self=this;  
-        var _inputValue=this.refs.ksearchInput.value;//搜索框输入的关键字  
-        var _main=this.refs.kpiTree;
-         _self._renderTreeNode(_main,_self.state.data01,0);    
-        // _main.innerHTML="";  
-        // var searchListUrl = '../src/kpiTree/searchListData.json';  
-        // fetch(searchListUrl,{  
-        //     credentials:'same-origin',  
-        //     async:false,  
-        //     //method: 'POST',  
-        //     method: 'GET',  
-        //     mode:'cors',//跨域请求  
-        //     headers: {  
-        //         "Content-type": "application/x-www-form-urlencoded",  
-        //         "User-Agent": "  Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"  
-        //     },  
-        // })  
-        //     .then(function(res) {  
-        //         console.log("Response succeeded?", JSON.stringify(res.ok));  
-        //         return res.json();  
-        //     })  
-  
-        //     .then(function(data) {  
-        //         debugger  
-        //         _self._renderTreeNode(_main,data,0);  
-        //     })  
-        //     .catch(function(e) {  
-        //         console.log("fetch fail",e.toString());  
-        //     });  
-    }  
-    _handleReturn=()=>{  
-        this.refs.ksearchInput.value="";//清空搜索输入框  
-        let _main=this.refs.kpiTree;  
-        _main.innerHTML="";  
-        this._fetchTreeNodeData("",_main,0);  
-    }  
-    _handleSelect=()=>{   
-        let _select=[];  
-        $(this.refs.kpiTree).find("input:checkbox").each(function() {//$('#kpiTree input:checkbox')  
-            if ($(this)[0].pid!=undefined&&$(this)[0].checked ==true) {  
-                let _selected_kpi={};  
-                _selected_kpi.kid=$(this)[0].parentNode.id.substring(4);  
-                _selected_kpi.kname=$(this)[0].parentNode.children[1].innerHTML  
-                _selected_kpi.pid=$(this)[0].parentNode.pid.substring(4);  
-                _select.push(_selected_kpi);
-            }  
-        });  
-        this.props.callbackParent(_select);  
     }  
     componentDidMount=()=>{  
         let _main=this.refs.kpiTree;  
         this._fetchTreeNodeData("",_main,0); 
         // this._fetchTreeNodeData(nodeId,currentNode,20);
-    }  
-    _selectAllCheckBox=(parentNodeId,event)=>{  
-        let _items=$("#" + parentNodeId+" input")  
-        for(let i=0;i<_items.length;i++){  
-            if (_items[i].pid!=undefined&&_items[i].pid==parentNodeId){  
-                _items[i].checked=event.currentTarget.checked;  
-            }  
-        }  
-    }  
+    } 
     _renderTreeNode=(nodeObj,treeData,paddingLeft)=>{  
         let _self=this;  
         let hasAllSelectBox=false; 
@@ -123,35 +101,33 @@ export default class KpiTree extends Component{
                 _div.style.paddingLeft=paddingLeft+"px";  
                 let _i=document.createElement("i");    
                 _i.className="add square icon";  
-                _i.onclick=_self._handleClick.bind(this,data.kid); 
-                let _checkBox=document.createElement("input");  
-                _checkBox.type="checkbox"  
-                _checkBox.pid="node"+data.pid;  
+                // _i.onclick=_self._handleClick.bind(this,data.kid); 
+                // let _checkBox=document.createElement("input");  
+                // _checkBox.type="checkbox"  
+                // _checkBox.pid="node"+data.pid;  
                 let _span=document.createElement("span");  
+                _span.onclick=_self._handleClick.bind(this,data.kid); 
                 _span.innerHTML=_kname;  
-                let allCheckBoxDiv=null;  
-                if(data.pid!="-1"&&index==0&&hasAllSelectBox==true){  
-                    allCheckBoxDiv=document.createElement("div");  
-                    allCheckBoxDiv.pid="node"+data.pid;  
-                    allCheckBoxDiv.style.paddingLeft=paddingLeft+"px";  
-                    let _allCheckBox=document.createElement("input");  
-                    _allCheckBox.type="checkbox";  
-                    _allCheckBox.onchange=_self._selectAllCheckBox.bind(this,nodeObj.id);  
-                    let _allCheckBoxLabel=document.createElement("span");  
-                    _allCheckBoxLabel.innerHTML="全选";  
-                    allCheckBoxDiv.appendChild(_allCheckBox);  
-                    allCheckBoxDiv.appendChild(_allCheckBoxLabel);  
-                }  
-                if(data.hasChild=="1"){  
+                // let allCheckBoxDiv=null;  
+                // if(data.pid!="-1"&&index==0&&hasAllSelectBox==true){  
+                //     allCheckBoxDiv=document.createElement("div");  
+                //     allCheckBoxDiv.pid="node"+data.pid;  
+                //     allCheckBoxDiv.style.paddingLeft=paddingLeft+"px";  
+                //     let _allCheckBox=document.createElement("input");  
+                //     _allCheckBox.type="checkbox";  
+                //     _allCheckBox.onchange=_self._selectAllCheckBox.bind(this,nodeObj.id);  
+                //     let _allCheckBoxLabel=document.createElement("span");  
+                //     _allCheckBoxLabel.innerHTML="全选";  
+                //     allCheckBoxDiv.appendChild(_allCheckBox);  
+                //     allCheckBoxDiv.appendChild(_allCheckBoxLabel);  
+                // }  
+                if(data.hasChild=="1"){
                     _div.appendChild(_i);  
                 }  
                 else if(data.hasChild=="0"){  
-                    _div.appendChild(_checkBox);  
+                    _div.appendChild(_span);
                 }  
-                _div.appendChild(_span);  
-                if(allCheckBoxDiv){  
-                    nodeObj.appendChild(allCheckBoxDiv)  
-                }  
+                _div.appendChild(_span); 
                 nodeObj.appendChild(_div);  
             })  
   
@@ -159,15 +135,17 @@ export default class KpiTree extends Component{
     }  
     _fetchTreeNodeData=(nodeId,nodeObj,paddingLeft)=>{   
         let _self=this,cData;
-        if( nodeId=='' ){
-            cData = _self.state.data;
-        }else if( nodeId=='1' ){
-            cData = _self.state.data1
-        }else if( nodeId=='01' ){
-            cData = _self.state.data01
-        }else if( nodeId=='011' ){
-            cData = _self.state.data011
-        }
+        let datas = 'data' + nodeId;
+        cData = _self.state[datas];
+        // if( nodeId=='' ){
+        //     cData = _self.state.data;
+        // }else if( nodeId=='1' ){
+        //     cData = _self.state.data1
+        // }else if( nodeId=='01' ){
+        //     cData = _self.state.data01
+        // }else if( nodeId=='011' ){
+        //     cData = _self.state.data011
+        // }
         _self._renderTreeNode(nodeObj,cData,paddingLeft);  
         // var treeListUrl = '../src/kpiTree/treeListData'+nodeId+'.json';  
         // fetch(treeListUrl,{  
@@ -195,19 +173,22 @@ export default class KpiTree extends Component{
         //     });  
   
     }  
-    _handleClick=(nodeId,event)=>{  
+    _handleClick=(nodeId,event)=>{
+        console.log(nodeId)
         let cData;
-        let _clickImg=event.currentTarget  
+        let _clickImg=event.currentTarget.previousElementSibling;  
         let currentNode=event.currentTarget.parentNode;
-        if( nodeId=='1' ){
-            cData = this.state.data
-        }else if( nodeId=='01' ){
-            cData = this.state.data1
-        }else if( nodeId=='011' ){
-            cData = this.state.data01
-        }
+        let datas='data' + nodeId.substr(1,(nodeId.length-1))
+        // if( nodeId=='1' ){
+        //     cData = this.state.data
+        // }else if( nodeId=='01' ){
+        //     cData = this.state.data1
+        // }else if( nodeId=='011' ){
+        //     cData = this.state.data01
+        // }
+        cData = this.state[datas];
         let index=currentNode.id.substr(currentNode.id.length-1,1)
-        console.log(cData[index-1])
+        // console.log(cData[index-1])
         this.setState({thisData:cData[index-1]});
         if(_clickImg.className.indexOf("square")!=-1){  
             _clickImg.className="minus icon";
